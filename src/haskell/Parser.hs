7edf1d1ad3a983efee05290bbd6e8fcc40b41960
OCDkirby <mangacreeperyt@gmail.com>
2022-04-09 19:36:51 -0500

    Complete B option quads

--- src/haskell/Parser.hs
+++ src/haskell/Parser.hs
@@ -5,8 +5,6 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE CApiFFI #-} -- Allows better imports with capi as opposed to ccall
 
-
-
 module Parser where
 
 import Foreign.C.Types
@@ -28,7 +26,6 @@ import TypeDeclarations
 import Helpers
 import CGets
 import Assembly
-import Helpers (symbolFromToken)
 
 foreign import capi "syntan/interface.h temp_add" tempAdd :: IO (Ptr Symbol)
 foreign import capi "syntan/interface.h block_add" blockAdd :: IO (Ptr Symbol)
@@ -49,7 +46,7 @@ precedence_matrix = [[   Skip, Yields,  Error,  Error,  Error,  Error, Yields,  
                      [  Error,  Error, Yields, Yields,  Takes, Yields,  Error,  Takes,  Error,  Error,  Error,  Error,  Error,  Error,  Takes,  Error,  Error,  Error,  Error,  Error ], -- relop
                      [   Skip, Yields,  Error,  Error,  Error,  Error, Yields,  Error,  Error,  Error, Yields,  Equal, Yields, Yields,  Error,  Error,  Error, Yields, Yields, Yields ], -- {
                      [  Error,  Takes,  Error,  Error,  Error,  Error,  Takes,  Error,  Error,  Error,  Takes,  Takes,  Takes,  Takes,  Error,  Error,  Takes,  Takes,  Takes,  Takes ], -- }
-                     [  Error,  Error,  Error, Yields,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error ], -- CALL
+                     [  Error,  Error,  Error,  Equal,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error,  Error ], -- CALL
                      [  Error,  Error, Yields, Yields,  Error, Yields,  Error,  Error, Yields, Yields,  Error,  Error,  Error,  Error,  Equal,  Error,  Error,  Error,  Error,  Error ], -- WHILE
                      [  Takes, Yields, Yields,  Error,  Error, Yields, Yields,  Error,  Error,  Error, Yields,  Takes, Yields, Yields,  Error,  Error,  Error, Yields,  Error, Yields ], -- DO
                      [  Takes,  Takes,  Takes,  Takes,  Takes,  Takes,  Error,  Error,  Error,  Error,  Error,  Takes,  Error,  Error,  Error,  Takes,  Error,  Error,  Error,  Error ], -- ,
@@ -87,14 +84,16 @@ toQuad [Right src, Left op, Right tf] symbols = do
   return $ QuadQQS op src tf temp                      -- +, src, tf, temp
 toQuad [Left op, Left src] symbols = do
   src_sym <- symbolFromToken src symbols
-  temp <- tempAdd >>= peek
-  return $ QuadSS op src_sym temp                      -- ODD, src, -, temp
+  return $ QuadS op src_sym                            -- ODD, src, -, -
 toQuad [Left op, Right src] symbols = do
-  temp <- tempAdd >>= peek
-  return $ QuadQS op src temp                          -- ODD, src, -, temp
+  return $ QuadQ op src                                -- ODD, src, -. -
+toQuad [Left op, Left src, Left lp, Left rp] symbols |  tok_class op == intEnum CALL
+                                                     && tok_class lp == intEnum LP
+                                                     && tok_class rp == intEnum RP = do
+  src_sym <- symbolFromToken src symbols
+  return $ QuadS op src_sym
 toQuad [Left iwop, Right cond, Left thenop, Right stmt] symbols = do
   return $ QuadIW iwop cond stmt
--- TODO: block pattern match, generateASM for all quads inside
 toQuad (Block (Left lb) quads (Left rb)) symbols |  tok_class lb == intEnum LB
                                                  && tok_class rb == intEnum RB = do
   generateASM quads
@@ -167,57 +166,71 @@ generateQuad stk@(tok_or_quad:rest) quad_stk symbols counter
 
 -- Lookup and push/pop logic
 -- TODO: when PROCEDURE pushed, add to stack to assign to next block
-stateFunc :: Token -> Stack TokenOrQuad -> [Symbol] -> IO (Bool, Stack TokenOrQuad)
-stateFunc tok [Left start] symbols = return (False, [Left tok, Left start]) -- Only a semi in the stack
-stateFunc tok stk symbols = do
+-- TODO: cur_idx needs to be idx . fromJust $ filterTop tokens isTerminal, not idx $ Left tok
+stateFunc :: [Token] -> Stack TokenOrQuad -> [Symbol] -> IO (Bool, Stack TokenOrQuad)
+stateFunc (tok:_) [Left start] _ = return (False, [Left tok, Left start]) -- Only a semi in the stack
+stateFunc tokens@(tok:_) stk symbols = do
   -- Index in table of current token
-  let cur_idx = fromIntegral . idx $ Left tok
+  --let cur_idx = fromIntegral . idx . Left . fromJust $ filterTop tokens isTerminalToken
+  let cur_idx = idx $ Left tok
 
   -- Index in table of top stack operator
   let stk_idx = fromIntegral . maybe (-1) idx $ filterTop stk isTerminal
 
-  if (stk_idx == 17 || stk_idx == 19) && cur_idx /= 0 -- Declaration statement
-     then do 
-    putStrLn "no push"
-    putStrLn ""
-    return (False, stk)
-  else if cur_idx < 0 || stk_idx < 0 then do -- tok is nonterminal or there are no operators in stk
-    putStrLn "unconditional push"
-    putStrLn ""
-    return (False, push stk $ Left tok)
+  if stk_idx == toIndex RB || isBlock (top stk) then do
+    putStrLn "Unconditional pop"
+    new_stk <- generateQuad stk [] symbols 0
+    return (True, new_stk)
   else do
-    print $ fromIndex stk_idx
-    print $ fromIndex cur_idx
-    
-    let action = precedence_matrix!!fromIntegral stk_idx!!fromIntegral cur_idx
 
-    if action == Yields || action == Equal then do
-      -- TODO: fixup
-      putStrLn "conditioned push"
+    if (stk_idx == 17 || stk_idx == 19) && cur_idx /= 0 -- Declaration statement
+      then do 
+      putStrLn "no push"
       putStrLn ""
-      return (False, push stk (Left tok))
-    else if action == Takes then do
-      putStrLn "Starting quad gen"
-      new_stk <- generateQuad stk [] symbols 0
-      if   tok_class tok == intEnum SEMI
-        || tok_class tok == intEnum DO
-        || tok_class tok == intEnum THEN
-        || tok_class tok == intEnum XPROC
-        || tok_class tok == intEnum RP 
-        || tok_class tok == intEnum RB then do -- Closing/collapsible token
-        putStrLn "Retry closer tok "
-        return (True, new_stk)
-      else return (False, new_stk)
-    else if action == Skip then do
-      putStrLn "Skippin"
       return (False, stk)
+    else if cur_idx < 0 || stk_idx < 0 then do -- tok is nonterminal or there are no operators in stk
+      putStrLn "unconditional push"
+      putStrLn ""
+      return (False, push stk $ Left tok)
     else do
-      putStrLn "tinvalid"
-      return (False, push stk (Left $ Token { tname = nullPtr, tok_class = intEnum TINVALID })) -- to be caught by F
+      print $ fromIndex stk_idx
+      print $ fromIndex cur_idx
+      
+      let action = precedence_matrix!!fromIntegral stk_idx!!fromIntegral cur_idx
+
+      if action == Yields || action == Equal then do
+        -- TODO: fixup
+        putStrLn "conditioned push"
+        putStrLn ""
+        return (False, push stk (Left tok))
+      else if action == Takes then do
+        putStrLn "Starting quad gen"
+        new_stk <- generateQuad stk [] symbols 0
+        if   tok_class tok == intEnum SEMI
+          || tok_class tok == intEnum DO
+          || tok_class tok == intEnum THEN
+          || tok_class tok == intEnum XPROC
+          || tok_class tok == intEnum RP 
+          || tok_class tok == intEnum RB then do -- Closing/collapsible token
+          putStrLn "Retry closer tok "
+          return (True, new_stk)
+        else return (False, new_stk)
+      else if action == Skip then do
+        putStrLn "Skippin"
+        return (False, stk)
+      else do
+        putStrLn "tinvalid"
+        return (False, push stk (Left $ Token { tname = nullPtr, tok_class = intEnum TINVALID })) -- to be caught by F
 
 
 -- Check if stack contains a valid program or errored
 checkEnd :: Stack TokenOrQuad -> (Bool, Stack TokenOrQuad)
+checkEnd (Program (Left semi) (Left xclass) (Left cname) (Left lb) quads (Left rb))
+  |  tok_class rb     == intEnum RB
+  && tok_class lb     == intEnum LB
+  && tok_class cname  == intEnum IDENT
+  && tok_class xclass == intEnum XCLASS
+  && tok_class semi   == intEnum SEMI = (True, quads)
 checkEnd (Left tok:rest) | tok_class tok == intEnum TINVALID = (True, Left tok:rest)
                          | otherwise = (False, Left tok:rest)
 checkEnd (Right Invalid:rest) = (True, Right Invalid:rest)
@@ -228,11 +241,12 @@ checkEnd any = (False, any)
 --  and taken over the info field
 -- Send beekeepers and debuggers
 pushDown :: P_D_Automaton symbol token stack_el -> [symbol] -> (token -> IO ()) -> [token] -> IO (Bool, Stack stack_el)
+pushDown (_, stk, f) _ _ [] = return . f $ reverse stk
 pushDown (delta, stk, f) symbols printer tokens@(tok:rest) = do
   printer tok
   putStrLn ""
 
-  (retry, new_stack) <- delta tok stk symbols 
+  (retry, new_stack) <- delta tokens stk symbols 
   let (end, err_stack) = f new_stack
 
   if end then return (end, err_stack)
@@ -251,7 +265,9 @@ runParser = do
   -- Pass and begin parse
   semi_str <- withCString ";" $ \ x -> do -- Stack-memory C string
     let semi_tok = Token { tname = x, tok_class = intEnum SEMI }
-    pushDown (stateFunc, [Left semi_tok], checkEnd) symbols printToken tokens
+    (valid, err_stack) <- pushDown (stateFunc, [Left semi_tok], checkEnd) symbols printToken tokens
+    if valid then putStrLn "Syntan: Parse Success"
+    else putStrLn "Syntan: Parse Fail"
   
   -- Clean up environment (lots of memory was thrown around during imports)
   -- NOTE clean up signal token symbol and quad
